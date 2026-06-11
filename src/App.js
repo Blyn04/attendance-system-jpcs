7973127bc68b9ecf2833dcb53005933b77fa7d11
berlene <berlenebernabe12@gmail.com>
2026-06-11 22:06:52 +0800

    chore: app.js update

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/main/*" element={<LayoutMain />} /> {/* Use wildcard to nest routes */}
+      <Route path="/main/*" element={<LayoutMain />} /> 
       {/* Remove these standalone routes since they are now part of LayoutMain */}
       {/* <Route path="/events" element={<CalendarComponent />} /> */}
       {/* <Route path="/attendance" element={<Attendance />} /> */}
-      <Route path="*" element={<Navigate to="/login" />} /> {/* Redirect any undefined paths */}
+      <Route path="*" element={<Navigate to="/login" />} /> 
     </Routes>
   );
 }
